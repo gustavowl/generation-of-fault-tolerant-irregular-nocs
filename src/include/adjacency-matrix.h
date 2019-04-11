5e92516174cf450346a6111fdf9ead0ab2a05906
Koruja <gustavowl@lcc.ufrn.br>
2019-04-10 22:58:14 -0300

    Creates adjacency-matrix.h file

---
+++ src/include/adjacency-matrix.h
@@ -0,0 +1,18 @@
+#ifndef __ADJACENCY_MATRIX__
+#define __ADJACENCY_MATRIX__
+
+// This class is used for representing square adjacency matrices.
+class AdjacencyMatrix {
+
+private:
+	unsigned int dim; //dimension of the square matrix
+
+public:
+	// Constructor
+	AdjacencyMatrix();
+
+	// Destructor
+	~AdjacencyMatrix(); 
+}
+
+#endif
--- src/include/file-manager.h
+++ src/include/file-manager.h
@@ -3,14 +3,19 @@
 
 #include <fstream>
 
+// This class is used for manipulating files
+// (Operations such as read and write)
+// TODO: define the format to be read: csv?
+// another notation?
 class FileManager {
-	public:
-		/* Attempts to read file in filepath location.
-		 * If reading fails, returns false
-		 * (e.g. the file does not exist)
-		 * and true otherwise. 
-		 * */
-		static bool readFile(std::string filepath);
+
+public:
+	/* Attempts to read file in filepath location.
+	 * If reading fails, returns false
+	 * (e.g. the file does not exist)
+	 * and true otherwise. 
+	 * */
+	static bool readFile(std::string filepath);
 };
 
 #endif
