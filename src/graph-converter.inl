6923097c4d7dc0d2c6b455706d36fa3412c73b39
Koruja <gustavowl@lcc.ufrn.br>
2019-04-19 21:29:12 -0300

    Implements Graph Converter

--- src/graph-converter.inl
+++ src/graph-converter.inl
@@ -1,5 +1,19 @@
 template <class ST, class DT>
 void GraphConverter::convert(GraphRepresentation<ST>* source,
 		GraphRepresentation<DT>* dest) {
-	//TODO
+	//Asserts that both representations have the same
+	//number of nodes.
+	unsigned int snodes = source->getNumNodes();
+	unsigned int dnodes = dest->getNumNodes();
+	if (snodes != dnodes)
+		return;
+
+	//Copies edges
+	for (unsigned int i = 0; i < snodes; i++) {
+		for (unsigned int j = 0; j < snodes; j++) {
+			if (! source->edgeExists(i, j))
+				continue;
+			dest->addEdge(i, j, source->getEdgeValue(i, j));
+		}
+	}
 }
--- src/include/graph-converter.h
+++ src/include/graph-converter.h
@@ -5,6 +5,19 @@
 
 class GraphConverter {
 public:
+	/* Converts a graph representation to another.
+	 * It copies the data from source to dest.
+	 * This method uses operator= to create the
+	 * deep copy. Thus, override this operator
+	 * in the DT (dest type) class if necessary.
+	 *
+	 * This method asserts that both representations
+	 * have the same number of nodes, then copies
+	 * the edges. It supposes that dest has no edges.
+	 * Thus, it does not delete dest edges.
+	 * If dest has edges, they may be overwritten
+	 * throughout the process.
+	 */
 	template <class ST, class DT> //Source Type, Dest Type
 	static void convert(GraphRepresentation<ST>* source, GraphRepresentation<DT>* dest);
 
