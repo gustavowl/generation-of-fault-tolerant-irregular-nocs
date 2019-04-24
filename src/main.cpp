79352d0e5bbb9d4237c659a7d752f1dae5e0a6d2
Koruja <gustavowl@lcc.ufrn.br>
2019-04-23 22:05:34 -0300

    Reads command line parameters

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,30 @@
 int main(int argc, char *argv[]) {
 	std::cout << "Hello World" << std::endl;
 
-//	GraphRepresentation<size_t>* gr = FileManager::readFile(
-//			"task-graphs/adj-lists/descriptions_seed-73_multi-start-node_weight-range-1-100_dimension-4x4_in-out-degrees-2-2.adjl",
-//			FileManager::FileType::adj_list);
+	if (argc != 6) {
+		std::cout << "Invalid number of arguments! Five expected:\n" <<
+			"\t1 - Path to .adjl file;\n" <<
+			"\t2 - Value separator (',' is the standard);\n" <<
+			"\t3 - Number of edges in the output graph (epsilon);\n" <<
+			"\t4 - Tabu list size;\n" <<
+			"\t5 - Number of iterations with no improvements " <<
+			"(stop criteria)" << std::endl;
+		return -1;
+	}
+
+	std::cout << "Reading \"" << argv[1] << "\" file..." << std::endl;
 
 	GraphRepresentation<size_t>* gr = FileManager::readFile(
-			"dijkstra.adjl", FileManager::FileType::adj_list, ' ');
+			argv[1], FileManager::FileType::adj_list, argv[2][0]);
 
-	if (gr == NULL)
+	if (gr == NULL) {
+		std::cout << "Failed to read \"" << argv[0] <<
+			"\" file using separator '" << argv[1][0] <<
+			"'." << std::endl;
 		return -1;
+	}
+
+	std::cout << "Done." << std::endl;
 
 	AdjacencyMatrix<size_t> adjm = AdjacencyMatrix<size_t>(
 			gr->getNumNodes(), false, false, 0);
@@ -37,13 +52,22 @@ int main(int argc, char *argv[]) {
 	std::cout << gr->getNumEdges() << ' ' << adjm.getNumEdges() <<
 		std::endl;
 
+	std::cout << "Begins Tabu Search..." << std::endl;
 	//initializes seed for pseudo-random number generation
 	srand(time(NULL));
 	AdjacencyMatrix<size_t>* res = TabuSearch<size_t>::start(
-			gr, 1, 1000, 9);
+			gr, std::stoi(argv[4]), std::stoi(argv[5]),
+				std::stoi(argv[3]));
+
+	delete gr;
+
+	if (res == NULL) {
+		std::cout << "Error running Tabu Search.\nProbably invalid " <<
+			"epsilon or tabu list size values" << std::endl;
+		return -1;
+	}
 
 	delete res;
-	delete gr;
 
 	return 0;
 }
