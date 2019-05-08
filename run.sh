39590673d18ae71444eccf1df1af1d3f1aadcd73
Koruja <gustavowl@lcc.ufrn.br>
2019-05-08 14:43:47 -0300

    Adaps target graphs' format

--- run.sh
+++ run.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-SEPARATOR=' '
-GRAPHS=("test.adjl" "dijkstra.adjl" "task-graphs/jonathan-graphs/AP1/AP1_prog.txt")
+SEPARATOR=','
+GRAPHS=("task-graphs/jonathan-graphs-adap/ap1.adjl"\
+	"task-graphs/jonathan-graphs-adap/ap2.adjl"\
+	"task-graphs/jonathan-graphs-adap/ap3.adjl"\
+	"task-graphs/jonathan-graphs-adap/ap4.adjl"\
+	"task-graphs/jonathan-graphs-adap/integral.adjl"\
+	"task-graphs/jonathan-graphs-adap/mpeg.adjl"\
+	"task-graphs/jonathan-graphs-adap/mwd.adjl"\
+	"task-graphs/jonathan-graphs-adap/vopd.adjl")
 STOP_CRITERION=(100 250 500 1000 2000)
 NUM_NODE_MULT=(0.5 1 2 3 4 5)
+
+#task-graphs/adj-lists/descriptions_seed-73_single-start-node_weight-range-1-100_dimension-4x4_in-out-degrees-2-2.adjl
+#descriptions_seed-73_single-start-node_weight-range-1-100_dimension-4x4_in-out-degrees-3-3.adjl
 
 sc_count=1
 for sc in ${STOP_CRITERION[*]}; do
 	graph_count=1
 	for graph in ${GRAPHS[*]}; do
 		ghead=$(head -1 $graph)
-		nodeindex=$(expr index "$ghead" ' ')
-		numnodes=${ghead:0:$nodeindex}
+		nodeindex=$(expr index "$ghead" "$SEPARATOR")
+		numnodes=${ghead:0:$nodeindex-1}
 		numedges=${ghead:nodeindex}
 
 		TABU_LIST_SIZES=(1)
@@ -25,20 +35,20 @@ for sc in ${STOP_CRITERION[*]}; do
 			for (( epsilon=$numnodes; epsilon<=$numnodes*2; epsilon++ )); do
 				exec_count=1
 				for i in {1..30}; do
-					echo "=============================================================================="
+					echo "===================================================================================="
 					echo "Stop Criterion:" $sc '('$sc_count'/'${#STOP_CRITERION[*]}')'
 					echo "Graph: "$graph '('$graph_count'/'${#GRAPHS[*]}')'
 					echo "Tabu List Size:" $tlsize '('$tlsize_count'/'${#TABU_LIST_SIZES[*]}')'
 					echo "Epsilon:" $epsilon '('$epsilon_count'/'$((numnodes+1))')'
 					echo "Execution:" $exec_count '('$exec_count'/'30')'
-					echo "------------------------------------------------------------------------------"
+					echo "------------------------------------------------------------------------------------"
 					otpt=$(echo `expr "$graph" : '\(\(\([^/]\)*/\)*\)'`)
 					pos=${#otpt}
 					otpt=${graph:pos}
 
-					./gftinoc $graph ' ' $epsilon $tlsize $sc $otpt
+					./gftinoc $graph $SEPARATOR $epsilon $tlsize $sc $otpt
 
-					echo "=============================================================================="
+					echo "===================================================================================="
 					echo -e "\n\n\n"
 					((exec_count++))
 				done
