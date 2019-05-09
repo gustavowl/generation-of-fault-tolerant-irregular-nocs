0a95d491063063509b35e48edcdcee7846a606df
Koruja <gustavowl@lcc.ufrn.br>
2019-05-08 21:59:04 -0300

    Updates run script

--- run.sh
+++ run.sh
@@ -16,57 +16,61 @@ NUM_NODE_MULT=(0.5 1 2 3 4 5)
 #descriptions_seed-73_single-start-node_weight-range-1-100_dimension-4x4_in-out-degrees-3-3.adjl
 
 sc_count=1
-for sc in ${STOP_CRITERION[*]}; do
-	graph_count=1
-	for graph in ${GRAPHS[*]}; do
-		ghead=$(head -1 $graph)
-		nodeindex=$(expr index "$ghead" "$SEPARATOR")
-		numnodes=${ghead:0:$nodeindex-1}
-		numedges=${ghead:nodeindex}
+for j in {0..2}; do
+	for sc in ${STOP_CRITERION[*]}; do
+		graph_count=1
+		for graph in ${GRAPHS[*]}; do
+			ghead=$(head -1 $graph)
+			nodeindex=$(expr index "$ghead" "$SEPARATOR")
+			numnodes=${ghead:0:$nodeindex-1}
+			numedges=${ghead:nodeindex}
 
-		TABU_LIST_SIZES=(1)
-		for i in ${NUM_NODE_MULT[*]}; do
-			TABU_LIST_SIZES+=($(echo "scale=0; (${numnodes}*${i})/1" | bc))
-		done
-
-		tlsize_count=1
-		for tlsize in ${TABU_LIST_SIZES[*]}; do
-			epsilon_count=1
-			for (( epsilon=$numnodes; epsilon<=$numnodes*2; epsilon++ )); do
-				exec_count=1
-				for i in {1..30}; do
-					echo "===================================================================================="
-					echo "Stop Criterion:" $sc '('$sc_count'/'${#STOP_CRITERION[*]}')'
-					echo "Graph: "$graph '('$graph_count'/'${#GRAPHS[*]}')'
-					echo "Tabu List Size:" $tlsize '('$tlsize_count'/'${#TABU_LIST_SIZES[*]}')'
-					echo "Epsilon:" $epsilon '('$epsilon_count'/'$((numnodes+1))')'
-					echo "Execution:" $exec_count '('$exec_count'/'30')'
-					echo "------------------------------------------------------------------------------------"
-					otpt=$(echo `expr "$graph" : '\(\(\([^/]\)*/\)*\)'`)
-					pos=${#otpt}
-					otpt=${graph:pos}
-					echo $otpt
-					otpt=$(echo `expr "$otpt" : '\(\([^\.]\)*\)'`)
-					otpt=$otpt"-sc"$sc"-tls"$tlsize"-eps"$epsilon"-exec"
-					if [ $i -lt 10 ]; then
-						otpt=$otpt'0'$i
-					else
-						otpt=$otpt$i
-					fi
-					otpt=$otpt".adjl"
-					echo $otpt
-
-					./gftinoc $graph $SEPARATOR $epsilon $tlsize $sc $otpt
-
-					echo "===================================================================================="
-					echo -e "\n\n\n"
-					((exec_count++))
-				done
-				((epsilon_count++))
+			TABU_LIST_SIZES=(1)
+			for i in ${NUM_NODE_MULT[*]}; do
+				TABU_LIST_SIZES+=($(echo "scale=0; (${numnodes}*${i})/1" | bc))
 			done
-			((tlsize_count++))
+
+			tlsize_count=1
+			for tlsize in ${TABU_LIST_SIZES[*]}; do
+				epsilon_count=1
+				for (( epsilon=$numnodes; epsilon<=$numnodes*2; epsilon++ )); do
+					exec_count=1
+					for i in {1..10}; do
+						echo "===================================================================================="
+						echo "Stop Criterion:" $sc '('$sc_count'/'${#STOP_CRITERION[*]}')'
+						echo "Graph: "$graph '('$graph_count'/'${#GRAPHS[*]}')'
+						echo "Tabu List Size:" $tlsize '('$tlsize_count'/'${#TABU_LIST_SIZES[*]}')'
+						echo "Epsilon:" $epsilon '('$epsilon_count'/'$((numnodes+1))')'
+						echo "Execution:" $exec_count '('$exec_count'/'30')'
+						echo "------------------------------------------------------------------------------------"
+						otpt=$(echo `expr "$graph" : '\(\(\([^/]\)*/\)*\)'`)
+						pos=${#otpt}
+						otpt=${graph:pos}
+						echo $otpt
+						otpt=$(echo `expr "$otpt" : '\(\([^\.]\)*\)'`)
+						otpt=$otpt"-sc"$sc"-tls"$tlsize"-eps"$epsilon"-exec"
+						mult=$(($j*10+$i))
+						if [ $mult -lt 10 ]; then
+							otpt=$otpt'0'$mult
+						else
+							otpt=$otpt$mult
+						fi
+						otpt=$otpt$mult
+						otpt=$otpt".adjl"
+						echo $otpt
+
+						./gftinoc $graph $SEPARATOR $epsilon $tlsize $sc $otpt
+
+						echo "===================================================================================="
+						echo -e "\n\n\n"
+						((exec_count++))
+					done
+					((epsilon_count++))
+				done
+				((tlsize_count++))
+			done
+			((graph_count++))
 		done
-		((graph_count++))
+		((sc_count++))
 	done
-	((sc_count++))
 done
